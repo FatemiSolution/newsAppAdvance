5b7365a5e230a96604812ff33899a41c792ed648
FatemiSolution <shabbir.high@gmail.com>
2023-10-21 18:59:06 +0500

    remove previous Navbar and NewsItem and paste new from Bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,96 +1,49 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-function Navbar() {
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = (props) => {
+  
+
   return (
-    <>
-        return (
-    <>
-      <nav
-        className={`navbar navbar-expand-lg navbar-${props.Mode==='light'?'light':'dark'} bg-body-primary sticky-top`}
-        style={{ backgroundColor: `${props.Mode==='light'?' #6499E9':'black'}` }}
-      >
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            Newsify
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link
-                  id="Home"
-                  className="nav-link"
-                  aria-current="page"
-                  to="/general"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link id="Business" className="nav-link" to="/business">
-                  Business
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  id="Entertainment"
-                  className="nav-link"
-                  to="/entertainment"
-                >
-                  Entertainment
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link id="Health" className="nav-link" to="/health">
-                  Health
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link id="Science" className="nav-link" to="/science">
-                  Science
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link id="Sports" className="nav-link" to="/sports">
-                  Sports
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link id="technology" className="nav-link" to="/technology">
-                  Technology
-                </Link>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                onChange={handleOnChange}
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-            </form>
-          <div className={`form-check form-switch text-${props.Mode==='light'?'dark':'light'}`}>
+    <nav className="navbar navbar-expand-lg bg-body-primary  sticky-top">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">NewSify</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/business">Business</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/health">Health</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/entertainment">Entertainment</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/sports">Sports</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/science">Science</a>
+          </li>
+        </ul>
+        <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        <div className={`form-check form-switch text-${props.Mode==='light'?'dark':'light'}`}>
             <input className="form-check-input" onClick={()=>{props.toggleMode()}} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
       </div>
-          </div>
-        </div>
-      </nav>
-    </>
-  ); 
-    </>
+      </div>
+    </div>
+  </nav>
   )
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,45 +2,18 @@ import React from 'react'
 
 function NewsItem(props) {
     // getting the props
-  let { title, description, imageUrl, newsUrl, publishedAt, author, source } =
+  let { title, description, imageUrl, newsUrl } =
   props;
   return (
     // format for the card froom bootstrap
-    <div className="my-4">
-      <div className="card" style={{ min_width: "100%",color: `${props.Mode==='light'?'black':'white'}`,backgroundColor: `${props.Mode==='light'?'white':'#454545'}` }}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "flex-end",
-            position: "absolute",
-            right: "0",
-          }}
-        >
-          <span className="  badge rounded-pill bg-danger">{source}</span>
-        </div>
-        <img
-          src={imageUrl}
-          width={150}
-          height={200}
-          className="card-img-top"
-          alt="..."
-        />
-        <div className="card-body">
-          <h5 className="card-title">{title}...</h5>
-          <p className="card-text">{description}...</p>
-          <p className="card-text" >
-            <small  style={{color: `${props.Mode==='light'?'#3C4048':'#B2B2B2'}`}}>
-              {" "}
-              {author != null ? "By " + author : ""} on{" "}
-              {new Date(publishedAt).toGMTString()}
-            </small>
-          </p>
-          <a href={newsUrl} className={`btn btn-sm btn-${props.Mode==='light'?'primary':'dark'}`}>
-            Read more
-          </a>
-        </div>
-      </div>
+    <div className="card" style={{width: '18rem'}}>
+    <img src={imageUrl} className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{description}</p>
+      <a href={newsUrl} className="btn btn-primary">Read More</a>
     </div>
+  </div>
   );
 }
 
